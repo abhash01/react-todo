27af766a404b3f0c21eba6716afb7871d038045b
abhash01 <abhash01@gmail.com>
2021-01-03 17:59:21 +0530

    Updates

---
+++ precache-manifest.d121dc06f87ec31e28fbd36e140d7dd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d5b2f98953d9bfe37f9f1df3cda254",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "019fb7f1b5ddea1e75fb",
+    "url": "/react-todo/static/css/main.4d8ab585.chunk.css"
+  },
+  {
+    "revision": "0176acb17efa2db9e345",
+    "url": "/react-todo/static/js/2.bfe32cf0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-todo/static/js/2.bfe32cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019fb7f1b5ddea1e75fb",
+    "url": "/react-todo/static/js/main.c382f519.chunk.js"
+  },
+  {
+    "revision": "57e7bc0f7e0e99026078",
+    "url": "/react-todo/static/js/runtime-main.1091c8cd.js"
+  }
+]);
